8835d891831c7083f615c1e1eb915558c4bdde05
Your Name <you@example.com>
2020-05-25 09:21:48 +0530

    added google foobar braille conveter

---
+++ Braille_converter.py
@@ -0,0 +1,42 @@
+def solution(s):
+    # Your code here
+    # Finding the braille letter of english letter
+    braille_converter = {
+        "a": "100000",
+        "b": "110000",
+        "c": "100100",
+        "d": "100110",
+        "e": "100010",
+        "f": "110100",
+        "g": "110110",
+        "h": "110010",
+        "i": "010100",
+        "j": "010110",
+        "k": "101000",
+        "l": "111000",
+        "m": "101100",
+        "n": "101110",
+        "o": "101010",
+        "p": "111100",
+        "q": "111110",
+        "r": "111010",
+        "s": "011100",
+        "t": "011110",
+        "u": "101001",
+        "v": "111001",
+        "w": "010111",
+        "x": "101101",
+        "y": "101111",
+        "z": "101011",
+        "cap": "000001",
+        " ": "000000"
+    }
+    final = ""
+    for l in s:
+        if l.isupper():
+            final += braille_converter["cap"]
+        if l is ' ':
+            final += braille_converter[" "]
+        else:
+            final += braille_converter[l.lower()]
+    return final
